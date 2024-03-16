ab8bd9aba3346cd7a9269d6795e9ef06ba2ac91f
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-03-16 16:09:05 +0900

    天才ルールベース

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,26 @@ fn main() {
     let mut source = LineSource::new(BufReader::new(stdin.lock()));
     input! {
         from &mut source,
-        fs: [Usize1; N],
+        fs: [Usize1; N * N],
     }
-    for _ in 0..N * N {
+    for i in 0..N * N {
         input! {
             from &mut source,
             p: Usize1,
         }
-        println!("{}", 'F');
+        println!(
+            "{}",
+            if i == N * N - 1 {
+                'F'
+            } else {
+                match (fs[i], fs[i + 1]) {
+                    (_, 0) => 'F',
+                    (0, _) => 'B',
+                    (_, 1) => 'R',
+                    (_, 2) => 'L',
+                    (_, _) => 'F',
+                }
+            }
+        );
     }
 }
